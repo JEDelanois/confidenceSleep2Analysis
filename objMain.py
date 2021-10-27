d2b517a7b892bcb782d9e921e7314e51c78bec30
Erik Delanois <jedelanois@gmail.com>
2021-10-27 14:07:21 -0700

    added code to analyze class report

--- objMain.py
+++ objMain.py
@@ -46,13 +46,49 @@ Utils.ConfigUtil.loadConfigsForSimulations(data)
     # for datasetName in datasetNames:
         # Metrics.Basics.plotMetric(data, datsetName=datasetName, metricName=metricName)
 
+# metricNames = ["TP", "TN", "FP", "FN", "precision", "recall", "F1", "accuracy", "loss", "matlabAccuracy"] 
+# metricFiles = ["TFPN/TP.txt", "TFPN/TN.txt", "TFPN/FP.txt", "TFPN/FN.txt", "TFPN/precision.txt", "TFPN/recall.txt", "TFPN/F1Score.txt", "accuracy.txt", "loss.txt", "matlabAccuracy.txt"] 
+# datasetNames = ["task1TrainData", "task2TrainData"]
+
 metricNames = ["TP", "TN", "FP", "FN", "precision", "recall", "F1", "accuracy", "loss", "matlabAccuracy"] 
 metricFiles = ["TFPN/TP.txt", "TFPN/TN.txt", "TFPN/FP.txt", "TFPN/FN.txt", "TFPN/precision.txt", "TFPN/recall.txt", "TFPN/F1Score.txt", "accuracy.txt", "loss.txt", "matlabAccuracy.txt"] 
-datasetNames = ["task1TrainData", "task2TrainData"]
+datasetNames = ["task1TrainData", "task1TrainDataBlur2", "task1TrainDataBlur10", "task1TrainDataBlur20", "task1TrainDataBlur40"]
 
 for metricName, metricFile in zip(metricNames, metricFiles):
     Metrics.LoadData.loadMetric(data, metricName=metricName, metricFile=metricFile, forceDatasetLoadFolders=datasetNames, detectMemberDataFolders=False)
     Metrics.Basics.plotTrialMetrics(data, datsetNames=datasetNames, metricNames=[metricName])
     Metrics.Basics.barTrialMetrics(data, datsetNames=datasetNames, metricNames=[metricName], plotIdxs=[0,-1], xticks=["Post Training", "Post Sleep"])
+
+# for total dataset
+classNames = [str(x) for x in range(4)]
+classNames.extend(["microavg", "macroavg", "weightedavg"])
+metricSuffixs = ["precision", "recall", "f1-score"]
+metricNames = []
+metricFiles = []
+for className in classNames:
+    for metricSuffix in metricSuffixs:
+        metricNames.append("%s-%s" % (className, metricSuffix))
+        metricFiles.append("%s-%s.txt" % (className, metricSuffix))
+
+# load all class report
+datasetNames = ["classReport"]
+for metricName, metricFile in zip(metricNames, metricFiles):
+    Metrics.LoadData.loadMetric(data, metricName=metricName, metricFile=metricFile, forceDatasetLoadFolders=datasetNames, detectMemberDataFolders=False)
+
+# load individual class reports
+tempDatasetNames = ["task1TrainData", "task1TrainDataBlur2", "task1TrainDataBlur10", "task1TrainDataBlur20", "task1TrainDataBlur40"]
+metricFiles = []
+for className in classNames:
+    for metricSuffix in metricSuffixs:
+        metricFiles.append("/classReport/%s-%s.txt" % (className, metricSuffix))
+
+for metricName, metricFile in zip(metricNames, metricFiles):
+    Metrics.LoadData.loadMetric(data, metricName=metricName, metricFile=metricFile, forceDatasetLoadFolders=tempDatasetNames, detectMemberDataFolders=False)
+
+datasetNames.extend(tempDatasetNames)
+
+for metricName in metricNames:
+    Metrics.Basics.plotTrialMetrics(data, datsetNames=datasetNames, metricNames=[metricName])
+    Metrics.Basics.barTrialMetrics(data, datsetNames=datasetNames, metricNames=[metricName], plotIdxs=[0,-1], xticks=["Start Training", "End Training"])
 
 data.saveFigures()
