fe0d2f0b89f93eac8f136b9ea33d7806a1263916
Erik Delanois <jedelanois@gmail.com>
2022-08-05 09:11:34 -0700

    modified analysis for genetic algo

--- Metrics/LoadData.py
+++ Metrics/LoadData.py
@@ -1,6 +1,7 @@
 
 from os import wait
 import Utils.DataUtil
+import numpy as np
 
 def loadClassMetric2(data):
     print("Loading ClassMetric2")
@@ -10,7 +11,7 @@ def loadConfidences(data):
     print("Loading Confidences")
     loadMetric(data, metricName="confidence", metricFile="confidenceForResponsibleCell.txt")
 
-def loadMetric(data, metricName="confidence", metricFile="confidenceForResponsibleCell.txt", forceDatasetLoadFolders=["Training"], detectMemberDataFolders=False):
+def loadMetric(data, metricName="confidence", metricFile="confidenceForResponsibleCell.txt", forceDatasetLoadFolders=["Training"], detectMemberDataFolders=False, ensure2d=True):
     for sim in data.sims:
         for trial in sim.trials:
             if  not hasattr(trial.data, 'datasetMetrics') or trial.data.datasetMetrics is None:
@@ -26,9 +27,13 @@ def loadMetric(data, metricName="confidence", metricFile="confidenceForResponsib
                             trial.data.datasetMetrics[memberName] = {}
                         filePath = trial.path + memberName + "/" + metricFile 
                         trial.data.datasetMetrics[memberName][metricName] = Utils.DataUtil.numpyDataQuickLoadEasy(filePath)
+                        if ensure2d and len(trial.data.datasetMetrics[memberName][metricName].shape) < 2:
+                            trial.data.datasetMetrics[memberName][metricName] = np.expand_dims(trial.data.datasetMetrics[memberName][metricName], axis=0)
 
             for memberName in forceDatasetLoadFolders:
                 if  memberName not in trial.data.datasetMetrics:
                     trial.data.datasetMetrics[memberName] = {}
                 filePath = trial.path + memberName + "/" + metricFile 
                 trial.data.datasetMetrics[memberName][metricName] = Utils.DataUtil.numpyDataQuickLoadEasy(filePath)
+                if ensure2d and len(trial.data.datasetMetrics[memberName][metricName].shape) < 2:
+                    trial.data.datasetMetrics[memberName][metricName] = np.expand_dims(trial.data.datasetMetrics[memberName][metricName], axis=0)
